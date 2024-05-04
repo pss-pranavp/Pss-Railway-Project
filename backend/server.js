ff0bf7089b19fbd31f4225b6ade4f0c0dd6984a0
Pranav Patil <166120450+pss-pranavp@users.noreply.github.com>
2024-05-04 15:33:26 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = 8081;
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "railwaydb"
+});
+
+app.options("/", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.send();
+});
+
+
+app.post('/', async (req, res) => {
+    console.log("Received POST request:", req.body);
+    const sql = "INSERT INTO pressedoffwheels (`Date`, `ShoOperratorTNo`, `InspectorT`, `ShopSNo`, `TypeofWheel`, `WheelPressedofffor`, `DiscSrNo`, `AxleNo`, `Reason`, `Remarks`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    const values = [req.body.Date, req.body.ShoOperratorTNo, req.body.InspectorT, req.body.ShopSNo, req.body.TypeofWheel, req.body.WheelPressedofffor, req.body.DiscSrNo, req.body.AxleNo, req.body.Reason, req.body.Remarks];
+    db.query(sql, values, (err, result) => {
+        if (err)
+            return res.json({ message: "Something unexpected has occured" + err });
+        return res.json({ success: "Add Data Sucessfully" });
+    });
+});
+
+
+app.get("/", (req, res) => {
+    const sql = "SELECT * FROM pressedoffwheels";
+    db.query(sql, (err, data) => {
+        if (err) {
+            console.error("Error executing SQL query:", err);
+            return res.status(500).json({ error: "An error occurred while fetching data." });
+        }
+        return res.json(data);
+    });
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is runing on port ${port}`);
+});
